eab5c0e04b813a7a47217f3344c5a0f1a3ac9384
aashu79 <aashusah435@gmail.com>
2025-12-21 15:39:20 +0545

    initial commit

---
+++ MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Daily_Journal_App
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
